38aea0d5d301b798d4741d0a9a1e3ded659129a2
protoop <protoop@users.noreply.github.com>
2015-05-18 22:55:35 +0200

    Updated __init__.py

--- app/newspapers/fr/__init__.py
+++ app/newspapers/fr/__init__.py
@@ -1,7 +1,13 @@
-__all__ = ["Challenges", "DirectMatin", "LaTribune", "LeFigaro", "LeMonde", "LeParisien", "Lequipe", "LesEchos",
-           "Lexpress", "Liberation", "MetroNews", "OuestFrance", "TF1", "VingtMinutes"]
+__all__ = ["BFMTV", "Capital", "Challenges", "DirectMatin", "Europe1", "FranceInter", "LaCroix", "LaTribune",
+           "LeFigaro", "LeMonde", "LeParisien", "Lequipe", "LesEchos", "Lexpress", "Lhumanite", "Liberation",
+           "MetroNews", "NouvelObs", "OuestFrance", "TF1", "ValeursActuelles","VingtMinutes"]
+import BFMTV
+import Capital
 import Challenges
 import DirectMatin
+import Europe1
+import FranceInter
+import LaCroix
 import LaTribune
 import LeFigaro
 import LeMonde
@@ -9,8 +15,11 @@ import LeParisien
 import Lequipe
 import LesEchos
 import Lexpress
+import Lhumanite
 import Liberation
 import MetroNews
+import NouvelObs
 import OuestFrance
 import TF1
+import ValeursActuelles
 import VingtMinutes
