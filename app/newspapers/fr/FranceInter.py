d24ddb526a502d017171119d862d5a4ea437e960
protoop <protoop@users.noreply.github.com>
2015-05-18 22:55:02 +0200

    Added a newspaper (France Inter).

--- app/newspapers/fr/FranceInter.py
+++ app/newspapers/fr/FranceInter.py
@@ -6,8 +6,8 @@ from app.utils import ScraperHelper, Database
 class Scraper:
 
     def __init__(self):
-        self.name = 'Challenges'
-        self.homepage_url = "http://www.challenges.fr"
+        self.name = 'France Inter'
+        self.homepage_url = "http://www.franceinter.fr"
         self.watched_urls = []
         self.db = Database.NewspapersDatabase()
         if not self.db.is_newspaper_in_database(self.name):
@@ -21,8 +21,8 @@ class Scraper:
         soup = sc.getSoupFromPage(self.homepage_url)
 
         # Articles
-        articles_une = soup.select(".obs-une article .obs-article-title a")
-        articles_others = soup.select(".obs-headlines .obs-article .title a")
+        articles_une = soup.select(".item-une .title a")
+        articles_others = soup.select(".item-fleuve h3 a")
 
         articles = articles_une + articles_others
 
@@ -48,31 +48,21 @@ class Scraper:
     def keep_track_of(self, url):
         self.watched_urls.append(url)
 
-    def encode_to_timecode(self, raw_time):
-        if raw_time == '':
-            return ''
-        if len(raw_time) == 19:
-            raw_time = raw_time + "+02:00"
-        return raw_time
-
     def get_article_infos_and_log_into_DB(self,url):
         # Scraper
         sc_article = ScraperHelper.ScraperHelper()
         soup_article = sc_article.getSoupFromPage(url)
         try:
-            article_title = soup_article.select("h1")[0].text.strip()
+            if len(soup_article.select(".entete h1")) > 0:
+                article_title = soup_article.select(".entete h1")[0].text.strip()
+            else:
+                article_title = soup_article.select(".entete h2")[0].text.strip()
         except:
             article_title = soup_article.title.string.strip()
-        try:
-            article_creation_time = soup_article.find(attrs={"itemprop":"datePublished"})['content']
-        except:
-            article_creation_time = ''
-        article_creation_time = self.encode_to_timecode(article_creation_time)
-        try:
-            article_update_time = soup_article.find(attrs={"itemprop":"dateModified"})['content']
-        except:
-            article_update_time = ''
-        article_update_time = self.encode_to_timecode(article_update_time)
+        # No time !!! (in almost each case, or too "dirty")
+        article_creation_time = ''
+        # No time !!! (in almost each case, or too "dirty")
+        article_update_time = ''
         try:
             if len(soup_article.find_all("meta", attrs={"property":"og:image"})) > 0:
                 article_image_url = soup_article.find_all("meta", attrs={"property":"og:image"})[0]['content']
@@ -82,7 +72,9 @@ class Scraper:
             article_image_url = ''
 
         # (No error in the breadcrumb's class name)
-        article_themes = [link.text.strip() for link in soup_article.select(".obs-breadcrumbs a")
-                                                if ('Challenges' != link.text.strip())]
+        article_themes = [link.text.strip() for link in soup_article.select(".date .noir")]
+        if len(article_themes) == 0:
+            article_themes = [img['title'] for img in soup_article.select(".une .title img") if img.has_attr('title')]
 
+        # The data gathered are *very* dirty...
         self.db.log_article_to_database(self.newspaper_id, url, article_title, article_themes, article_creation_time, article_update_time, article_image_url)
