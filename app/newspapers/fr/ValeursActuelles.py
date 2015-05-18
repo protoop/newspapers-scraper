977d526a0ec4418af2b66b8351c0fdc5cb336b97
protoop <protoop@users.noreply.github.com>
2015-05-18 22:52:03 +0200

    Added a newspaper (Valeurs Actuelles).

--- app/newspapers/fr/ValeursActuelles.py
+++ app/newspapers/fr/ValeursActuelles.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 from __future__ import unicode_literals
 import datetime
+import re
 from app.utils import ScraperHelper, Database
 
 class Scraper:
 
     def __init__(self):
-        self.name = "Europe 1"
-        self.homepage_url = "http://www.europe1.fr"
+        self.name = "Valeurs Actuelles"
+        self.homepage_url = "http://www.valeursactuelles.com"
         self.watched_urls = []
         self.db = Database.NewspapersDatabase()
         if not self.db.is_newspaper_in_database(self.name):
@@ -21,8 +22,7 @@ class Scraper:
         soup = sc.getSoupFromPage(self.homepage_url)
 
         # Articles
-        articles = soup.select("#news article a")
-        articles += soup.select(".popular .wrapper a")
+        articles = soup.select(".post h2 a")
 
         articles_urls = list(set([article['href'] for article in articles if article.has_attr('href')]))
 
@@ -46,20 +46,35 @@ class Scraper:
     def keep_track_of(self, url):
         self.watched_urls.append(url)
 
-    def encode_to_timecode(self, raw_time):
+    def encode_to_timecode_from_creation_text(self, raw_time):
         if raw_time == '':
             return ''
-        if len(raw_time) == 20:
-            raw_time = raw_time[0:19] + "+02:00"
+        days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+        months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre',
+                      'Novembre', 'Décembre']
+        for day in days:
+            if day in raw_time:
+                raw_time = raw_time.replace(day, "").strip()
+        for idx, month in enumerate(months):
+            if month in raw_time:
+                raw_time = raw_time.replace(month, "%02d" % ((idx + 1),))
+        if len(raw_time) == 18:
+            #we have sth like : 18 05 2015 à 15:17
+            #we want sth like : 2015-05-18T15:17:00+02:00
+            raw_time = raw_time[6:10] + '-' + raw_time[3:5] + '-' + raw_time[0:2] + 'T'\
+                       + raw_time[13:18] + ":00+02:00"
+        else:
+            raw_time = ''
         return raw_time
 
-    def encode_to_timecode_from_le_lab(self, raw_time):
+    def encode_to_timecode_from_update_text(self, raw_time):
         if raw_time == '':
             return ''
-        #we have sth like : 2015/05/18 16:48:06
-        #we want sth like : 2015-05-18T16:48:06+02:00
-        if len(raw_time) == 19:
-            raw_time = raw_time[0:10].replace("/", "-") + "T" + raw_time[11:19] + "+02:00"
+        if len(raw_time) == 18:
+            #we have sth like : 18/05/2015 à 16:48
+            #we want sth like : 2015-05-18T16:48:00+02:00
+            raw_time = raw_time[6:10] + '-' + raw_time[3:5] + '-' + raw_time[0:2] + 'T'\
+                       + raw_time[13:18] + ":00+02:00"
         else:
             raw_time = ''
         return raw_time
@@ -69,35 +84,31 @@ class Scraper:
         sc_article = ScraperHelper.ScraperHelper()
         soup_article = sc_article.getSoupFromPage(url)
         try:
-            article_title = soup_article.select("h1")[0].text.strip()
+            article_title = soup_article.select("h2")[0].text.strip()
         except:
             article_title = soup_article.title.string.strip()
+        text_date = soup_article.select(".date")[0].text.strip()
         try:
-            if 'lelab.' not in url:
-                article_creation_time = soup_article.find(attrs={"itemprop":"datePublished"})['datetime']
-                article_creation_time = self.encode_to_timecode(article_creation_time)
-            else:
-                article_creation_time = soup_article.find(attrs={"itemprop":"datePublished"})['data-date']
-                article_creation_time = self.encode_to_timecode_from_le_lab(article_creation_time)
+            # sth like : Lundi 18 Mai 2015 à 15:17
+            creation_text = re.search('(.+?) \(mis à jour le', text_date).group(1)
+            article_creation_time = self.encode_to_timecode_from_creation_text(creation_text)
         except:
             article_creation_time = ''
         try:
-            if 'lelab.' not in url:
-                article_update_time = soup_article.find(attrs={"itemprop":"dateModified"})['datetime']
-                article_update_time = self.encode_to_timecode(article_update_time)
-            else:
-                article_update_time = soup_article.find(attrs={"itemprop":"dateModified"})['data-date']
-                article_update_time = self.encode_to_timecode_from_le_lab(article_update_time)
+            # sth like : 18/05/2015 à 16:48
+            update_text = re.search('mis à jour le (.+?)\)', text_date).group(1)
+            article_update_time = self.encode_to_timecode_from_update_text(update_text)
         except:
             article_update_time = ''
         try:
-            if len(soup_article.find_all("meta", attrs={"property":"og:image"})) > 0:
-                article_image_url = soup_article.find_all("meta", attrs={"property":"og:image"})[0]['content']
+            if len(soup_article.find_all("img", attrs={"typeof":"foaf:Image"})) > 0:
+                article_image_url = soup_article.find_all("img", attrs={"typeof":"foaf:Image"})[0]['src']
             else:
-                article_image_url = soup_article.find_all("meta", attrs={"property":"og:image:url"})[0]['content']
+                article_image_url = ''
         except:
             article_image_url = ''
 
-        article_themes = [link.text.strip() for link in soup_article.select(".breadcrumb a")]
+        article_themes = [link.text.strip() for link in soup_article.select(".breadcrumbs a")
+                                                if link.text.strip() != 'Accueil']
 
         self.db.log_article_to_database(self.newspaper_id, url, article_title, article_themes, article_creation_time, article_update_time, article_image_url)
